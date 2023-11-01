c1731b3185ae7b0d7ecdce1c568965961121d0f7
yuri.egawa <yuri.egawa@magazine.co.jp>
2023-11-01 16:58:43 +0900

    Authフロントエンド部分作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
-import { ProductListComponent } from './product/product-listings/product-listings.component';
-import { ProductDetailComponent } from './product/product-detail/product-detail.component';
-import { ProductComponent } from './product/product.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
